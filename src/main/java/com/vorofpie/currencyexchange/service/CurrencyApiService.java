fccc8ba23437aaada41bc9255570f06c3e625938
Matvey Voropay <vorofpie@gmail.com>
2024-07-18 19:10:04 +0300

    Add CurrencyApiService

--- src/main/java/com/vorofpie/currencyexchange/service/CurrencyApiService.java
+++ src/main/java/com/vorofpie/currencyexchange/service/CurrencyApiService.java
@@ -1,12 +1,9 @@
 package com.vorofpie.currencyexchange.service;
 
-import com.vorofpie.currencyexchange.domain.CurrencyRate;
-
 import java.time.LocalDate;
 
 public interface CurrencyApiService {
 
     void loadRates(LocalDate date);
 
-    CurrencyRate loadRateByCode(String currencyCode, LocalDate date);
 }
