532ab2eed7d2600423a4e92085f1b98224b9b665
Matvey Voropay <vorofpie@gmail.com>
2024-07-18 22:31:10 +0300

    Add pages

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    flatpickr('#loadDate', {
+        dateFormat: 'Y-m-d'
+    });
+    flatpickr('#getDate', {
+        dateFormat: 'Y-m-d'
+    });
+});
